675eedb9673de01b589097f5f0b6e9c43e26e978
jyothikaalla024 <jyothikaalla024@gmail.com>
2026-02-11 20:27:27 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,29 @@
-DB_HOST=database-1.co3kus82asfr.us-east-1.rds.amazonaws.com
-DB_USER=admin
-DB_PASSWORD=GPX86Gif3xasvFVrUdCq
-DB_NAME=database-1
-DB_PORT=3306
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+// Test DB connection
+const connect = async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("Order Service DB connected");
+    connection.release();
+  } catch (err) {
+    console.error("Database connection failed:", err.message);
+    process.exit(1); // Optional: exit container if DB fails
+  }
+};
+
+module.exports = {
+  pool,
+  connect
+};
