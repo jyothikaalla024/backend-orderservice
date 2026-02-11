20c3ffef549323ff987b165fb228013990f4e2cf
jyothikaalla024 <jyothikaalla024@gmail.com>
2026-02-11 20:23:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require("express");
+const cors = require("cors");
+ 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
-
+ 
+app.use(cors({
+  origin: "https://amznpro.online", // replace with real frontend domain
+}));
+ 
 app.get("/orders", (req, res) => {
   res.json([
     { orderId: 101, product: "Laptop" },
     { orderId: 102, product: "Phone" }
   ]);
 });
-
+ 
 app.get("/health", (req, res) => {
   res.send("Order Service is healthy");
 });
-
-app.listen(PORT, () =>
-  console.log(`Order Service running on port ${PORT}`)
-);
+ 
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Order Service running on port ${PORT}`);
+});
