e4dd4b630db428c9124237357045b3edbd268306
jyothikaalla024 <jyothikaalla024@gmail.com>
2026-03-11 15:34:26 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,16 +1,15 @@
 const mysql = require('mysql2');
+require('dotenv').config();
+
 const pool = mysql.createPool({
-  host: 'database-1.co3kus82asfr.us-east-1.rds.amazonaws.com',
-  user: 'admin',
-  password: '0pEaAr1PODRsddq2Ur0w',
-  database: 'orderdb',   // make sure this database exists
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-module.exports = {
-  pool: pool.promise(),   // so you can use async/await with db.pool.query
-  connect: () => console.log('Order Service DB connected')
-};
+module.exports = pool.promise();
