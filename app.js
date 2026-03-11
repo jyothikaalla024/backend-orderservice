c6f9c869aec6be4dcbe0a344aace313da686e574
jyothikaalla024 <jyothikaalla024@gmail.com>
2026-03-11 15:26:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,16 +36,13 @@ app.use(express.json());
 // ========== DATABASE TEST ==========
 (async () => {
   try {
-await db.pool.query("SELECT 1");
+    await db.query("SELECT 1");
+    console.log("✅ MySQL Database connected (Order Service)");
+  } catch (err) {
+    console.error("❌ MySQL connection failed:", err.message);
+  }
+})();
 
-
-const [rows] = await db.pool.query("SELECT * FROM orders");
-
-// Insert order
-const [result] = await db.pool.query(
-  "INSERT INTO orders (user_id, product_name, amount) VALUES (?, ?, ?)",
-  [user_id, product_name, amount]
-);
 // ========== ROUTES ==========
 
 // Get all orders
