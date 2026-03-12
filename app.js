a95ca92421c3305680f079bafe94617f7da16869
jyothikaalla024 <jyothikaalla024@gmail.com>
2026-03-12 14:35:22 +0530

    Fix formatting of express import in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const express = require("express");
+const express = require("express"); 
 const cors = require("cors");
 const compression = require("compression");
 const db = require("./db");
